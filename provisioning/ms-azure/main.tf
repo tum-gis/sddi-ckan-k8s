50d6742b71ca4bfbd6840e6f5576923427c2ecba
Bruno Willenborg <b.willenborg@tum.de>
2023-02-13 14:46:14 +0100

    cleanup and use location form variables

--- provisioning/ms-azure/main.tf
+++ provisioning/ms-azure/main.tf
@@ -1,7 +1,7 @@
 # Local variable definitions --------------------------------------------------
 locals {
   # Service region
-  location = "West Europe"
+  location = var.location
 
   # Node ressource group name
   resource_group_nodes_name = format("%s-nodes", var.resource_group_name)
@@ -15,8 +15,7 @@ locals {
 
   # Common tags
   tags = {
-    partner    = "TUM-GI"
-    subproject = "MSA"
+    someTag    = "someValues"
   }
 }
 
@@ -26,26 +25,3 @@ resource "azurerm_resource_group" "this" {
   location = local.location
   tags     = local.tags
 }
-
-# # Create a virtual network within the resource group --------------------------
-# module "network" {
-#   source              = "Azure/network/azurerm"
-#   resource_group_name = azurerm_resource_group.this.name
-#   address_space       = "10.162.0.0/16"
-#   subnet_prefixes     = ["10.162.1.0/24"]
-#   subnet_names        = ["subnet-aks"]
-#   depends_on          = [azurerm_resource_group.this]
-#   tags = local.tags
-# }
-
-# With separate subnet for pods
-# module "network" {
-#   source              = "Azure/network/azurerm"
-#   resource_group_name = azurerm_resource_group.this.name
-#   address_spaces       = ["10.162.0.0/16"]
-#   subnet_prefixes     = ["10.162.1.0/24", "10.162.2.0/24"]
-#   subnet_names        = ["subnet-aks", "subnet-pods"]
-#   depends_on          = [azurerm_resource_group.this]
-
-#   tags = local.tags
-# }
