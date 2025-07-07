84d95a06000a7c9df4d29e6969ff7661548b2beb
Bruno Willenborg <me@brunowillenborg.de>
2025-07-07 17:28:19 +0200

    formatting

--- provisioning/ms-azure/outputs.tf
+++ provisioning/ms-azure/outputs.tf
@@ -6,27 +6,27 @@ output "resource_group_nodes_info" {
   value = data.azurerm_resource_group.k8s-node
 }
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.this.kube_config_raw
+  value     = azurerm_kubernetes_cluster.this.kube_config_raw
   sensitive = true
 }
 
 output "client_key" {
-  value = azurerm_kubernetes_cluster.this.kube_config.0.client_key
+  value     = azurerm_kubernetes_cluster.this.kube_config.0.client_key
   sensitive = true
 }
 
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.this.kube_config.0.client_certificate
+  value     = azurerm_kubernetes_cluster.this.kube_config.0.client_certificate
   sensitive = true
 }
 
 output "cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.this.kube_config.0.cluster_ca_certificate
+  value     = azurerm_kubernetes_cluster.this.kube_config.0.cluster_ca_certificate
   sensitive = true
 }
 
 output "host" {
-  value = azurerm_kubernetes_cluster.this.kube_config.0.host
+  value     = azurerm_kubernetes_cluster.this.kube_config.0.host
   sensitive = true
 }
 
@@ -35,7 +35,7 @@ output "cluster-fqdn" {
 }
 
 output "cluster-info" {
-  value = azurerm_kubernetes_cluster.this
+  value     = azurerm_kubernetes_cluster.this
   sensitive = true
 }
 
