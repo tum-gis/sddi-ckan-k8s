71c1f115a24cb04911fbc5255a5ac7198ce0375a
Bruno Willenborg <me@brunowillenborg.de>
2025-07-07 17:29:05 +0200

    formatting & tags

--- provisioning/ms-azure/main.tf
+++ provisioning/ms-azure/main.tf
@@ -7,16 +7,14 @@ locals {
   resource_group_nodes_name = format("%s-nodes", var.resource_group_name)
 
   # Budget name
-  budget_name = format("%s-budget", var.resource_group_name)
+  budget_name       = format("%s-budget", var.resource_group_name)
   budget_name_nodes = format("%s-budget-nodes", var.resource_group_name)
 
   # K8s service name
   k8s_service_name = var.resource_group_name
 
   # Common tags
-  tags = {
-    someTag    = "someValues"
-  }
+  tags = var.tags
 }
 
 # Create a resource group -----------------------------------------------------
